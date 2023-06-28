b7ff0536dec2228c6b3bee854a09ac317382f8b8
ujeongoh <yujoh94@gmail.com>
2023-06-29 02:44:33 +0900

    fix : 파일명 변경, 에러 수정

--- dags/spotify_regional_weekly_chart.py
+++ dags/spotify_regional_weekly_chart.py
@@ -2,7 +2,10 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.empty import EmptyOperator
 from airflow.sensors.filesystem import FileSensor
+from airflow.providers.snowflake.operators.snowflake import SnowflakeOperator
+from airflow.providers.amazon.aws.sensors.s3 import S3KeySensor
 from airflow.decorators import task
+from airflow.utils.task_group import TaskGroup
 from airflow.hooks.base import BaseHook
 from airflow.models import Variable
 from selenium import webdriver
@@ -27,22 +30,20 @@ def delete_files(file_directory, file_pattern):
     for file in get_files(file_directory, file_pattern): 
         os.remove(file)  
         
-@task
-def get_csv_files(date):
-    countries = ["ar", "au", "at", "by", "be", "bo", "br", "bg", "ca", "cl", 
-             "co", "cr", "cy", "cz", "dk", "do", "ec", "ch", "cl", "de", 
-             "dk", "do", "ee", "eg", "es", "fi", "FR", "gb", "gr", "gt", 
-             "hk", "hn", "hu", "id", "ie", "il", "in", "is", "it", "jp", 
-             "kr", "kz", "lt", "lu", "lv", "ma", "mx", "my", "ng", "ni", 
-             "nl", "no", "nz", "pa", "pe", "ph", "pk", "pl", "pt", "py", 
-             "ro", "sa", "se", "sg", "sk", "sv", "th", "tr", "tw", "ua", 
-             "us", "uy", "ve", "vn", "za"]
+def _get_csv_files(date, file_directory, file_pattern):
+    # countries = ["ar", "au", "at", "by", "be", "bo", "br", "bg", "ca", "cl", 
+    #          "co", "cr", "cy", "cz", "dk", "do", "ec", "ch", "cl", "de", 
+    #          "dk", "do", "ee", "eg", "es", "fi", "FR", "gb", "gr", "gt", 
+    #          "hk", "hn", "hu", "id", "ie", "il", "in", "is", "it", "jp", 
+    #          "kr", "kz", "lt", "lu", "lv", "ma", "mx", "my", "ng", "ni", 
+    #          "nl", "no", "nz", "pa", "pe", "ph", "pk", "pl", "pt", "py", 
+    #          "ro", "sa", "se", "sg", "sk", "sv", "th", "tr", "tw", "ua", 
+    #          "us", "uy", "ve", "vn", "za"]
+    countries = ['us','kr']
 
     logging.info(f'date : {date}')
     
     logging.info('delete before saving new files')
-    file_directory = '/opt/downloads'
-    file_pattern = f"regional-*-weekly-{date}.csv"
     delete_files(file_directory, file_pattern)
     
     # Set webriver options
@@ -97,16 +98,12 @@ def get_csv_files(date):
                 count += 1
         logging.info(f'total {count} files downloaded')  
 
-@task
-def transform_and_combine_csv_files(source_directory, target_directory, file_pattern):
+def _transform_and_combine_csv_files(source_directory, target_directory, source_file_pattern, target_file_pattern):
     
     logging.info("create dataframe for integration")
     header = ['rank','track_id','artist_names','track_name','peak_rank','previous_rank','weeks_on_chart','streams', 'country_code','chart_date']
     combined_df = pd.DataFrame(columns=header)
-    files = get_files(source_directory, file_pattern)
-    fn = os.path.basename(files[0])
-    _, _, _, year, month, day = fn.split('-')
-    date = f'{year}-{month}-{day}'
+    files = get_files(source_directory, source_file_pattern)
 
     logging.info("loop files start")
     # 파일명을 기반으로 국가명과 일자 정보를 추출하여 데이터에 추가
@@ -138,15 +135,14 @@ def transform_and_combine_csv_files(source_directory, target_directory, file_pat
         combined_df = pd.concat([combined_df, df], ignore_index=True)
     logging.info("loop files end")
     
+    logging.info('delete before saving new files')
+    delete_files(target_directory, target_file_pattern)
+    
     logging.info("save combined csv file")
-    new_filename = f"countries_weekly_top200_{date}"
-    new_file_path = os.path.join(target_directory, new_filename)
+    new_file_path = os.path.join(target_directory, target_file_pattern)
     combined_df.to_csv(new_file_path, index=False)
     
-    return new_file_path
-    
-@task
-def upload_files_to_s3(file_directory, file_pattern, bucket_name, folder_name):
+def _upload_files_to_s3(file_directory, file_pattern, bucket_name, folder_name):
     
     logging.info("get aws connection")
     aws_conn = BaseHook.get_connection('aws_conn_id')
@@ -174,43 +170,119 @@ def upload_files_to_s3(file_directory, file_pattern, bucket_name, folder_name):
         logging.info(f"upload file : {s3_key}")
         with open(file, 'rb') as data:
             s3.upload_fileobj(data, bucket_name, s3_key)
-
-        logging.info(f"remove {file_name}")  
-        os.remove(file)  
         
-    logging.info("loop files end")
+    logging.info("loop files end")  
 
-      
+def _delete_uploaded_files(file_directory, file_pattern):
+    delete_files(file_directory, file_pattern)
 
 with DAG(
-    dag_id='get_spotify_chart_data',  
+    dag_id='spotify_regional_weekly_chart',  
     start_date=datetime(2023, 5, 25),
     schedule_interval=timedelta(days=7),  # 실행 간격
     catchup=True
 ) as dag:
-    # date = '2023-06-15'
     date = '{{ ds }}'
     bucket_name = 'kpop-analysis'
-    source_folder_name = f'weekly_chart/countries/{date}'
-    result_folder_name = f'weekly_chart/result/{date}'
+    source_folder_name = f'raw_data/spotify/chart/{date}'
+    target_folder_name = f'transformed_data/spotify/chart/{date}'
     source_directory = os.getenv('AIRFLOW_HOME') + "/downloads"
     target_directory = os.getenv('AIRFLOW_HOME') + "/data"
     source_file_pattern = f"regional-*-weekly-{date}.csv"
-    target_file_pattern = f"countries_weekly_top200_{date}.csv"
-    
+    target_file_pattern = f"regional-weekly-{date}.csv"
+    schema = 'raw'
+    table = 'regional_weekly_chart'
+
     start = EmptyOperator(
         task_id="start"
     )
     
-    # check_combined_file = FileSensor(
-    #     task_id='check_combined_file', 
-    #     filepath=os.getenv('AIRFLOW_HOME') + "/data"
-    # )
+    get_csv_files = PythonOperator(
+        task_id='get_csv_files',
+        python_callable=_get_csv_files,
+        op_args=[date, source_directory, source_file_pattern],  
+    )
+    
+    upload_raw_files_to_s3 = PythonOperator(
+        task_id='upload_raw_files_to_s3',
+        python_callable=_upload_files_to_s3,
+        op_args=[source_directory, source_file_pattern, bucket_name, source_folder_name],  
+    )
+    
+    with TaskGroup("process_raw_files", tooltip="process raw files") as process_raw_files:
+        transform_and_combine_csv_files = PythonOperator(
+            task_id='transform_and_combine_csv_files',
+            python_callable=_transform_and_combine_csv_files,
+            op_args=[source_directory, target_directory, source_file_pattern, target_file_pattern],  
+        )
+        
+        upload_transformed_files_to_s3 = PythonOperator(
+            task_id='upload_transformed_files_to_s3',
+            python_callable=_upload_files_to_s3,
+            op_args=[target_directory, target_file_pattern, bucket_name, target_folder_name],  
+        )
+        
+        check_transformed_file_exists = S3KeySensor(
+            task_id='check_transformed_file_exists',
+            bucket_key=f's3://{bucket_name}/{target_folder_name}/{target_file_pattern}',
+            aws_conn_id='aws_conn_id',
+            timeout=18 * 60 * 60,
+            poke_interval=10 * 60,
+        )
+        
+        transform_and_combine_csv_files >> upload_transformed_files_to_s3 >> check_transformed_file_exists 
+        
+    
+    load_s3_to_snowflake = SnowflakeOperator(
+        task_id='load_s3_to_snowflake',
+        sql=f"""
+            BEGIN;
+
+            CREATE TEMPORARY TABLE temp_table AS 
+            SELECT 
+                $1 AS rank, 
+                $2 AS track_id, 
+                $3 AS artist_names, 
+                $4 AS track_name, 
+                $5 AS peak_rank, 
+                $6 AS previous_rank, 
+                $7 AS weeks_on_chart, 
+                $8 AS streams, 
+                $9 AS country_code, 
+                $10 AS chart_date
+            FROM @{schema}.transformed_data_stage_csv/spotify/chart/{date}/{target_file_pattern};
+            
+            DELETE FROM {schema}.{table} 
+            WHERE DATE(chart_date) = '{date}'::DATE;
+
+            INSERT INTO {schema}.{table} 
+            SELECT t.* 
+            FROM temp_table t;
+
+            COMMIT;
+            """,
+            
+        snowflake_conn_id='snowflake_conn_id',
+        autocommit=False,
+    )
+    
+    with TaskGroup("delete_files_from_container", tooltip="Delete the files from the container after they have been successfully loaded into S3") as delete_files_from_container:
+        delete_uploaded_raw_files = PythonOperator(
+            task_id='delete_uploaded_files',
+            python_callable=_delete_uploaded_files,
+            op_args=[source_directory, source_file_pattern],  
+        )
+        
+        delete_uploaded_transformed_files = PythonOperator(
+            task_id='delete_uploaded_transformed_files',
+            python_callable=_delete_uploaded_files,
+            op_args=[target_directory, target_file_pattern],  
+        )
+        [delete_uploaded_raw_files, delete_uploaded_transformed_files]
+        
     
     end = EmptyOperator(
         task_id="end"
     )
     
-    start >> get_csv_files(date) >> transform_and_combine_csv_files(source_directory, target_directory, source_file_pattern) >> [upload_files_to_s3(source_directory, source_file_pattern, bucket_name, source_folder_name)] >> upload_files_to_s3(target_directory, target_file_pattern, bucket_name, result_folder_name) >> end
-    
-             
+    start >> get_csv_files >> [upload_raw_files_to_s3, process_raw_files] >> load_s3_to_snowflake >> delete_files_from_container >> end
