d48045f06d0e8876c665e959a983911e53fee108
ujeongoh <yujoh94@gmail.com>
2023-07-04 18:36:54 +0900

    fix : 테이블 명과 조인 소스 테이블 변경, refactor

--- dbt/models/analytics/kpop_summary.sql
+++
@@ -1,6 +0,0 @@
-SELECT chart.country_code, chart.artist_names, chart.track_name, audio.danceability, audio.energy, audio.loudness, audio.speechiness, audio.acousticness, audio.liveness, audio.valence, audio.tempo
-FROM {{ ref('src_track_ids') }} kpop
-JOIN {{ ref('src_chart_ids') }} chart ON kpop.id = chart.track_id
-JOIN {{ ref('src_audio_ids') }} audio ON kpop.id = audio.id
-WHERE kpop.id IN (SELECT track_id FROM {{ ref('src_matched_ids') }})
-
