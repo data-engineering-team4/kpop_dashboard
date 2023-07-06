e79a3e1d8335686e50d2e314666c814ae83a6174
ujeongoh <yujoh94@gmail.com>
2023-07-06 15:01:14 +0900

    fix : 로그 삭제 기준 수정

--- dags/delete_log_dag.py
+++ dags/delete_log_dag.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator # Import BashOperator
-from utils.common_util import KST_TZ
 
 default_args = {
     'owner': 'kpop',
@@ -12,16 +11,16 @@ default_args = {
 with DAG(
     dag_id='delete_logs_past_7days', 
     default_args=default_args,
-    start_date=datetime(2023, 7, 1, tzinfo=KST_TZ), 
+    start_date=datetime(2023, 7, 1), 
     catchup=False, 
     schedule_interval='* 2 * * *',  # Crontime : min hour day month week / 매일 02시에 삭제
     tags=['operation']
 ) as dag:
 
-    # 7일 이상 이전인 로그 파일 찾아서 삭제
+    # 10일 이상 이전인 로그 파일 찾아서 삭제
     delete_logs = BashOperator(
         task_id='delete_logs',
-        bash_command='find /opt/airflow/logs -type f -mtime +7 -delete'
+        bash_command='find /opt/airflow/logs -type f -mtime +10 -delete'
     )
 
     delete_logs
