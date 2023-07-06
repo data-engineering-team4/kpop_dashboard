680e7bbb11087db7082188ae26ea4e39e56b766d
ujeongoh <yujoh94@gmail.com>
2023-07-06 14:40:52 +0900

    fix : task_id 특수문자 제거, 매주 일요일 실행스케줄 설정

--- dags/dbt_run_dag.py
+++ dags/dbt_run_dag.py
@@ -1,29 +1,31 @@
 import os
 import logging
 from datetime import datetime
-
 from airflow import DAG
-from airflow.operators.python import PythonOperator
-from operators.log_operator import LogOperator
 from airflow.operators.bash import BashOperator
-
-from utils.common_util import get_sql
+from utils.common_util import KST_TZ
 
 
 default_args = {
-    'start_date': datetime(2023, 6, 30),
+    'start_date': datetime(2023, 6, 30,tzinfo=KST_TZ),
     'catchup': False
 }
 
-with DAG('dbt_run_dag', schedule_interval='@once', default_args=default_args, tags=['dbt']) as dag:
-
-    task_1 = BashOperator(
-        task_id='run_dbt',
+with DAG(
+    dag_id='dbt_run_dag', 
+    schedule_interval='0 15 * * 0', 
+    default_args=default_args, 
+    tags=['dbt']
+) as dag:
+    
+    run_dbt = BashOperator(
+        task_id='run_dbt',
         bash_command='cd /dbt && dbt run --profiles-dir .',
         env={
             'DBT_ENV_SECRET_USER': '{{ var.value.dbt_user }}',
             'DBT_ENV_SECRET_PASSWORD': '{{ var.value.dbt_password }}',
             **os.environ
-        },
-        dag=dag
+        }
     )
+    
+    run_dbt
